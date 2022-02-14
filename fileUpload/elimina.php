1a213afd6ec9034b18b44fa4ee85fa0e6a04ed4c
LoreGerm <lorenzogermano146@gmail.com>
2022-02-14 10:59:55 +0100

    .

---
+++ fileUpload/elimina.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_GET['name'])) {
+    function Elimina($nome){
+                $directory = "file";
+                $images = glob($directory . "/" . $nome );
+            }
+        
+?>
--- fileUpload/index.php
+++ fileUpload/index.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
+<?php  require_once "elimina.php";  ?>
 <body class="container" style="background-color: #525252;">
     
     <form action="" method="POST" class="form" style="margin-top: 20px;" enctype="multipart/form-data">
@@ -46,16 +47,18 @@
                 echo 'error';
             }   
         }
-        
+
+?>
+
     ?>
 
     <div class="row" style="margin-top:50px">
                 <?php
                     $directory = "file";
                     $images = glob($directory . "/*.*");
-
                     foreach($images as $image){
-                        echo '<div class="col-3"><img src="'.$image.'"style="width:20rem"></div>';
+                        $nome = substr($image,7);
+                        echo '<div class="col-3"><img src="'.$image.'"style="width:20rem"> <a href="'elimina.php?nome=$nome'"><button type="button" class="btn btn-danger">Elimina</button></a> </div>';
                     }
                 ?>
     </div>
